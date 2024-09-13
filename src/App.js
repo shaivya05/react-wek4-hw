506648970a0d8d794cf23f0698a01203ed74db62
shaivya05 <130532388+shaivya05@users.noreply.github.com>
2024-09-13 14:24:06 +0530

    created a basic weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="searchForm">
+        <input type="search" className="search" placeholder="Enter  a  City" />
+        <input type="submit" value="Search" className="searchButton" />
+        <input type="submit" value="Current" className="currentButton" />
+      </form>
+      <div className="cityDetails">
+        <h3 className="name city">Lisbon </h3>
+        <p className="name">Thursday 11:42</p>
+        <p >Cloudy</p>
+      </div>
+      <div className="tempDetails">
+        <div className="tempValues">
+          <div className="details icon">☀️</div>
+          <div className="details value">39</div>
+          <div className="details degree">°C</div>
+        </div>
+        <div className="temp">
+          <div> Precipitation : 82 % </div>
+          <div> Wind : 4km/h</div>
+        </div>
+      </div>
     </div>
   );
 }
